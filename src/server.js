0ecbdeef4e38c08040b8a00c567b283a9fd43b13
Giheon <74302278+Dockerel@users.noreply.github.com>
2022-08-24 17:54:33 +0900

    wetube clone-22.08.24

---
+++ src/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed you are welcome");
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+
+app.get("/", handleHome);
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
