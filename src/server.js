1d9a3db99cf9e4414eac018784de8e9f2d3acd78
Giheon <74302278+Dockerel@users.noreply.github.com>
2022-08-25 17:55:47 +0900

    22.08.25 - morgan middlewares, routers, params

--- src/server.js
+++ src/server.js
@@ -1,30 +1,18 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed you are welcome");
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
-};
-
+const logger = morgan("dev");
 app.use(logger);
-app.use(privateMiddleware);
 
-app.get("/", handleHome);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);
