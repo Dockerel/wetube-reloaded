c410131b10188c178d2ea8a63d695dd998332baa
Giheon <74302278+Dockerel@users.noreply.github.com>
2022-09-12 14:46:24 +0900

    github login added

--- src/models/User.js
+++ src/models/User.js
@@ -3,9 +3,11 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  avatarUrl: String,
+  socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: false },
   location: String,
 });
 
