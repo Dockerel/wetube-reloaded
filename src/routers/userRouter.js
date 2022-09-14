f774a2cba3596f5b829153681fff55a9a908ea8f
Giheon <74302278+Dockerel@users.noreply.github.com>
2022-09-14 16:25:47 +0900

    upload system added

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -8,17 +8,33 @@ import {
   finishNaverLogin,
   getEdit,
   postEdit,
+  getChangePassword,
+  postChangePassword,
 } from "../controllers/userController";
-import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
+import {
+  avatarUpload,
+  protectorMiddleware,
+  publicOnlyMiddleware,
+} from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
-userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter
+  .route("/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(avatarUpload.single("avatar"), postEdit);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get("/naver/start", publicOnlyMiddleware, startNaverLogin);
 userRouter.get("/naver/finish", publicOnlyMiddleware, finishNaverLogin);
-userRouter.get(":id", see);
+userRouter
+  .route("/change-password")
+  .all(protectorMiddleware)
+  .get(getChangePassword)
+  .post(postChangePassword);
+
+userRouter.get("/:id", see);
 
 export default userRouter;
