e416f3ac0159e396085b979a9ab8d59041c3ad8f
Giheon <74302278+Dockerel@users.noreply.github.com>
2022-09-17 22:40:44 +0900

    css updated

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,1 +1,12 @@
 import "../scss/styles.scss";
+
+//upload.pug
+const fileText = document.querySelector(".file--text");
+const uploadFile = document.querySelector(".upload--btn");
+
+function handleChange(event) {
+  const fileName = event.target.files[0].name;
+  fileText.value = fileName;
+}
+
+uploadFile.addEventListener("change", handleChange);
