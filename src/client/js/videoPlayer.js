82cf122565b7a2b7ae7e8e339310e76393851a21
Giheon <74302278+Dockerel@users.noreply.github.com>
2022-09-19 22:25:18 +0900

    video player adding...

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,74 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.querySelector("#play");
+const muteBtn = document.querySelector("#mute");
+const volumeRange = document.querySelector("#volume");
+const currentTime = document.querySelector("#currentTime");
+const totalTime = document.querySelector("#totalTime");
+const timeline = document.querySelector("#timeline");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
+};
+
+const handleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+  } else if (!video.muted && volumeRange.value == 0) {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeValue = value;
+  video.volume = value;
+};
+
+const formatTime = (seconds) =>
+  new Date(Math.floor(seconds) * 1000).toISOString().substring(11, 19);
+
+const handleLoadedMeta = () => {
+  totalTime.innerText = formatTime(video.duration);
+  timeline.max = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+  currentTime.innerText = formatTime(video.currentTime);
+  timeline.value = Math.floor(video.currentTime);
+  if (video.currentTime === video.duration) {
+    video.paused = true;
+    playBtn.innerText = "Play";
+  }
+};
+
+const handleTimelineChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  video.currentTime = value;
+};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMeta);
+video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimelineChange);
